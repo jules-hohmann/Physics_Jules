945fecd51185d56a4f3b6cd7e8aa650f3349c2be
JulesHohmann <jules.hohmann@gmail.com>
2023-07-13 10:38:50 -0400

    added new test to willitfloat

--- test_physics.py
+++ test_physics.py
@@ -13,6 +13,7 @@ class TestPhysics(unittest.TestCase):
     def test_will_it_float(self):
         self.assertEqual(physics.will_it_float(10, 1), True)
         self.assertEqual(physics.will_it_float(1, 1001), False)
+        self.assertEqual(physics.will_it_float(1, 1000), True)
         self.assertEqual(physics.will_it_float(0, 1), ValueError)
         self.assertEqual(physics.will_it_float(1, 0), ValueError)
         self.assertEqual(physics.will_it_float(-1, 1), ValueError)
