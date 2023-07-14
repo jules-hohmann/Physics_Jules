7d3e51031f10cbba699ad9196ffdb02b7189f351
JulesHohmann <jules.hohmann@gmail.com>
2023-07-14 11:58:43 -0400

    solved problems 5-8, 8 testing was not finished

--- physics.py
+++ physics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # gravitaional force
 g = 9.81
@@ -6,31 +7,86 @@ g = 9.81
 w = 1000
 
 
-# calculates buoyancy based of density of fluid and Volume of object
-
-
 def calculate_buoyancy(p, V):
+    """calculates buoyancy based of density of fluid and Volume of object"""
+    """does not accept a pressure or Volume less than or equal to 0"""
     if p <= 0 or V <= 0:
         return ValueError
     buoyancy = p * V * g
     return buoyancy
 
 
-# uses Volume of object and mass to see if the object will float in water or not
 def will_it_float(V, m):
+    """uses Volume of object and mass to see if the object will float in water or not"""
+    """does not accept volume or mass of less than or equal to 0"""
     if V <= 0 or m <= 0:
         return ValueError
     gravitationalforce = m * g
     Totalforce = calculate_buoyancy(w, V) - gravitationalforce
-    if Totalforce >= 0:
+    if Totalforce > 0:
         return True
     if Totalforce < 0:
         return False
+    else:
+        return None
 
 
-# uses depth to find pressure in water
 def calculate_pressure(d):
+    """uses depth to find pressure in water"""
+    """does not accept a depth of less than 0"""
     if d < 0:
         return ValueError
     pressure = w * d * g
     return pressure
+
+
+def calculate_acceleration(F, m):
+    "" "takes force and mass to calculate the acceleration" ""
+    """Function cannot take a mass less than or equal to 0"""
+    if m <= 0:
+        return ValueError
+    a = F / m
+    return a
+
+
+def calculate_angular_acceleration(tau, I):
+    """uses the torque and moment of Inirtia to calculate angular acceleration"""
+    """Inertia cannot be negative or equal to 0 or the function will return an error"""
+    if I <= 0:
+        return ValueError
+    a = tau / I
+    return a
+
+
+def calculate_moment_of_inertia(m, r):
+    """uses the mass and the distance from axis of rotation to calculate Inertia"""
+    """neither mass or distance can be negative so the function returns error"""
+    if m <= 0 or r < 0:
+        return ValueError
+    I = m * np.power(r, 2)
+    return I
+
+
+mass = 100
+Volume = 0.1
+thrusterdistance = 0.5
+
+
+def calculate_auv_acceleration(F_magnitude, F_angle):
+    """takes the magnitude of force and angle of force to find the acceleration, returns a vector"""
+    Fx = 2 * np.cos(F_angle) * F_magnitude - 2 * np.sin(F_angle) * F_magnitude
+    Fy = 2 * np.sin(F_angle) * F_magnitude - 2 * np.cos(F_angle) * F_magnitude
+    ax = Fx / mass
+    ay = Fy / mass
+    acceleration = np.array[ax, ay]
+    return acceleration
+
+
+inertia = 1
+
+
+def calculate_auv_angular_acceleration(F_magnitude, F_angle):
+    """uses magnitude of force and angle to calculate acceleration"""
+    torque = np.sin(F_angle) * F_magnitude * thrusterdistance
+    a = inertia / torque
+    return a
--- test_physics.py
+++ test_physics.py
@@ -1,5 +1,6 @@
 import unittest
 import physics
+import math
 
 
 class TestPhysics(unittest.TestCase):
@@ -13,7 +14,7 @@ class TestPhysics(unittest.TestCase):
     def test_will_it_float(self):
         self.assertEqual(physics.will_it_float(10, 1), True)
         self.assertEqual(physics.will_it_float(1, 1001), False)
-        self.assertEqual(physics.will_it_float(1, 1000), True)
+        self.assertEqual(physics.will_it_float(1, 1000), None)
         self.assertEqual(physics.will_it_float(0, 1), ValueError)
         self.assertEqual(physics.will_it_float(1, 0), ValueError)
         self.assertEqual(physics.will_it_float(-1, 1), ValueError)
@@ -22,6 +23,27 @@ class TestPhysics(unittest.TestCase):
     def test_calculatepressure(self):
         self.assertEqual(physics.calculate_pressure(10), 98100)
         self.assertEqual(physics.calculate_pressure(-1), ValueError)
+
+    def test_calculate_acceleration(self):
+        self.assertEqual(physics.calculate_acceleration(10, 2), 5)
+        self.assertEqual(physics.calculate_acceleration(1, -1), ValueError)
+        self.assertEqual(physics.calculate_acceleration(1, 0), ValueError)
+
+    def test_calculate_angular_acceleration(self):
+        self.assertEqual(physics.calculate_angular_acceleration(6, 3), 2)
+        self.assertEqual(physics.calculate_angular_acceleration(1, -1), ValueError)
+        self.assertEqual(physics.calculate_angular_acceleration(1, 0), ValueError)
+
+    def test_calculate_moment_of_Inertia(self):
+        self.assertEqual(physics.calculate_moment_of_inertia(2, 2), 8)
+        self.assertEqual(physics.calculate_moment_of_inertia(-1, 1), ValueError)
+        self.assertEqual(physics.calculate_moment_of_inertia(0, 1), ValueError)
+        self.assertEqual(physics.calculate_moment_of_inertia(1, -1), ValueError)
+
+    def test_calculate_auv_acceleration(self):
+        self.assertEqual(
+            physics.calculate_auv_acceleration(1, math.pi),
+        )
 
 
 if __name__ == "__main__":
